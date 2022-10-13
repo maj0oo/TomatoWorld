5ab4c94a2c2e4808d457f59d96bf2277652d1274
maj0oo <michalmajecki2@gmail.com>
2022-10-13 22:12:35 +0200

    Initialize info added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Initialized");
     }
 
     // Update is called once per frame
